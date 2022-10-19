9e1576fb7197898027c66b2369cbbc3feb8f7210
mperezpizarro <marioperezpizarro@gmail.com>
2022-10-19 12:11:29 +0200

    Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 const Navbar: React.FC<Props> = ({children}) => {
   return (
-    <nav>
-        Navbar
+    <nav className='flex gap-x-6 justify-between'>
+        <h2 className='font-bold text-3xl text-black dark:text-white'>Lista de la compra 🛒</h2>
         {children}
     </nav>
   )
