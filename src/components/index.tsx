36af484a634ff71ef347b20b257a082f23150d21
mperezpizarro <marioperezpizarro@gmail.com>
2022-10-21 11:43:26 +0200

    Updated export statements

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,4 @@
 export {default as ListContainer} from './List/ListContainer'
 export {default as ListItem} from './List/ListItem'
 export {default as AddForm} from './Forms/AddForm' 
-export {default as SearchForm} from './Forms/SearchForm'
 export {default as Navbar} from './Navbar/Navbar'
