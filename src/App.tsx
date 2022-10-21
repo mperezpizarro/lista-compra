05412118b7c0ae9ab775b023c1fb9ab21f7bc8e4
mperezpizarro <marioperezpizarro@gmail.com>
2022-10-21 11:56:46 +0200

    Moved AddForm to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import { BsFillMoonStarsFill, BsFillSunFill, BsFillTrashFill } from 'react-icons
 
 const App = () => {
   const [isDark, setIsDark] = useState(false)
+  const [newItem, setNewItem] = useState('')
 
   const switchTheme = () => {setIsDark(!isDark)}
+
+  const addItem = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log('asasa');
+    
+  }
 
   let tmp = ['Agua', 'Pasta', 'Masa de pizza', 'Queso Chéddar', 'Coca Cola', 'Patatas']
 
@@ -23,6 +30,12 @@ const App = () => {
         <Navbar>
           <button className='text-xl p-4 rounded-full bg-[#00000011] dark:bg-[#ffffff11]' onClick={switchTheme}>{isDark ? <BsFillSunFill /> : <BsFillMoonStarsFill />}</button>
         </Navbar>
+        <form onSubmit={addItem}>
+          <div>
+            <input type="text" name="itemName" id="itemName" placeholder='¿Qué necesitas comprar?' />
+            <button type="submit">Añadir</button>
+          </div>
+        </form>
         <ListContainer>
           {tmp.map((objeto, index): JSX.Element => {
             return(
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,3 @@
 export {default as ListContainer} from './List/ListContainer'
 export {default as ListItem} from './List/ListItem'
-export {default as AddForm} from './Forms/AddForm' 
 export {default as Navbar} from './Navbar/Navbar'
